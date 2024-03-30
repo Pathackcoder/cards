b8ab1f00b727f07e8ced34364cace41d498126dd
Amit Pathak <91179150+Pathackcoder@users.noreply.github.com>
2024-03-30 14:58:16 +0530

    added script file

---
+++ script.js
@@ -0,0 +1,27 @@
+const cardData = [
+    { title: "NIMCET", content:"", imageUrl: "image3.png" },
+    { title: "CUET-PG", content:"", imageUrl: "image2.png" },
+    { title: "JAAMIA", content:"", imageUrl: "image1.png" },
+    // Add more data objects as needed
+  ];
+  
+  const cardContainer = document.getElementById("cardContainer");
+  
+  cardData.forEach(data => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+      <div class="card-header">
+        <img src="${data.imageUrl}" alt="${data.title}" class="card-image">
+        <h2 class="card-title">${data.title}</h2>
+      </div>
+      <div class="card-content">
+        <p>${data.content}</p>
+      </div>
+    `;
+    cardContainer.appendChild(card);
+    const cardImage = card.querySelector(".card-image");
+    cardImage.style.width = "40px"; // Set custom width
+    cardImage.style.height = "30px"; // Set custom height
+  });
+  
